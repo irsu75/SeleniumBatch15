02e4703064fbb3a4b163079316c3678ce6c91585
irsu75 <irinasukhanova01@gmail.com>
2023-02-22 13:25:23 -0500

    Initial commit

---
+++ src/HomeWork/Task1.java
@@ -0,0 +1,29 @@
+package HomeWork;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Task1 {
+    /*
+     * 1. launch the browser
+     * 2. navigate to amazon website
+     * 3. print out the title and the url in the console
+     * 4. close the browser
+     */
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver","Driver/chromedriver.exe");
+
+        WebDriver driver= new ChromeDriver();
+
+        driver.get("https://www.google.com");
+        driver.navigate().to("https://www.amazon.com");
+        String URL=driver.getCurrentUrl();
+        System.out.println(URL);
+        String title= driver.getTitle();
+        System.out.println(title);
+        driver.close();
+
+    }
+
+}
---
+++ src/Navigate.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Navigate {
+    public static void main(String[] args) throws InterruptedException {
+     // connect to webDriver
+        System.setProperty("webdriver.chrome.driver","Driver/chromedriver.exe");
+
+     // initate the instance of Webdriver
+        WebDriver driver=new ChromeDriver();
+
+       // open google.com
+       driver.get("https://www.google.com");
+
+       //now navigate to facebook.com
+        driver.navigate().to("https://www.facebook.com/");
+
+        //slow down
+        Thread.sleep(2000);
+
+        // go back
+        driver.navigate().back();
+
+        //go to facebook
+        driver.navigate().forward();
+        Thread.sleep(2000);
+
+       //refresh the page
+        driver.navigate().refresh();
+
+        //close the browser
+        driver.close();
+    }
+}
